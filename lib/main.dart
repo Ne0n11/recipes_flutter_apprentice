24746a278f92e56a50ca9f28af45d37de2131e28
Arthur Puha <pugaartur@gmail.com>
2021-10-07 13:44:27 +0300

    Section I complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'models/recipe.dart';
+import 'screens/recipe_detail.dart';
 
 void main() => runApp(RecipeApp());
 
@@ -47,7 +48,23 @@ class _MyHomePageState extends State<MyHomePage> {
           // 6
           itemBuilder: (BuildContext context, int index) {
             // 7
-            return buildRecipeCard(Recipe.samples[index]);
+            return GestureDetector(
+              // 8
+              onTap: () {
+                // 9
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      // 10
+                      return RecipeDetail(recipe: Recipe.samples[index]);
+                    },
+                  ),
+                );
+              },
+              // 11
+              child: buildRecipeCard(Recipe.samples[index]),
+            );
           },
         ),
       ),
@@ -55,20 +72,38 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget buildRecipeCard(Recipe recipe) {
-    // 1
     return Card(
+      // 1
+      elevation: 2.0,
       // 2
-      child: Column(
-        // 3
-        children: <Widget>[
-          // 4
-          Image(image: AssetImage(recipe.imageUrl)),
-          // 5
-          Text(recipe.label),
-        ],
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0)),
+      // 3
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        // 4
+        child: Column(
+          children: <Widget>[
+            Image(image: AssetImage(recipe.imageUrl)),
+            // 5
+            const SizedBox(
+              height: 14.0,
+            ),
+            // 6
+            Text(
+              recipe.label,
+              style: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Palatino',
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
+
 
 
 }
